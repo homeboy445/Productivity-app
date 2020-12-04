398bcda7b2774b44c0e3bb1b750803824b64288d
homeboy445 <akshitsan13@gmail.com>
2020-12-04 19:39:04 +0530

    Files have been added successfully!

---
+++ DeleteTask.js
@@ -0,0 +1,24 @@
+const fs=require('fs');
+const path=require('path');
+const {getData}=require(path.join(__dirname,"DataParser.js"));
+const DeleteTask=(task)=>{
+    var obj=getData();
+    var write_string='',id;
+    for(var i=0;i<obj.length;i++)
+    {
+        if(obj[i].task==task)
+        {
+            id=obj[i].id;
+            obj.splice(i,1);
+        }
+    }
+    obj.map(item=>{
+        write_string+=`([${item.date}]|${item.id}+${item.task}+${item.priority})*`;
+    })
+    fs.writeFile(path.join(__dirname,"TaskText.txt"),write_string,(e)=>{
+        return;
+    });
+}
+module.exports={
+    DeleteTask
+}
